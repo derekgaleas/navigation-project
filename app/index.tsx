5f27fca82803d5762f70ddfbbcf367e8687d079f
derekgaleas <derek.galeas@sanservices.hn>
2025-06-26 10:04:10 -0600

    style screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,19 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import CategoriesScreen from "../screens/CategoriesScreen";
 
 export default function Index() {
-  return <CategoriesScreen />;
+  return (
+    <View style={styles.container}>
+      <StatusBar style={"light"} />
+      <CategoriesScreen />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#24180f",
+  },
+});
